62095b4d5fc117916ba726706ca3818c18f82d49
Kyle Ideta <cubicberry@gmail.com>
2021-01-26 18:03:49 +0900

    Warn react/jsx-no-useless-fragment

--- react.js
+++ react.js
@@ -5,9 +5,9 @@ module.exports = {
     'react/jsx-boolean-value': 'warn',
     'react/jsx-curly-brace-presence': 'warn', // warn and auto fix for foo={'bar'}
     'react/jsx-fragments': 'warn',
+    'react/jsx-no-useless-fragment': 'warn',
     'react/jsx-key': ['warn', { checkFragmentShorthand: true }], // TODO: I want to disable for array literal, but is not supported for now. Let's PR to upstream.
     'react/self-closing-comp': 'warn',
-
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn', // want to make it error, but we should disable it for useEffect.
 
