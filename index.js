891121c476d3e5ad0edd6275059ef2c70177127e
Yuya Tanaka <yuya.presto@gmail.com>
2021-06-22 17:42:06 +0900

    Move ts rules into overrides to allow lint for js files

--- index.js
+++ index.js
@@ -3,16 +3,8 @@ const noRestrictedImportsRules = require('./noRestrictedImportsRules')
 module.exports = {
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/eslint-recommended',
-    'plugin:@typescript-eslint/recommended',
     'prettier',
-    "prettier/@typescript-eslint",
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    sourceType: 'module',
-    project: './tsconfig.json',
-  },
   plugins: ['prettier', '@typescript-eslint/eslint-plugin'],
   rules: {
     'prettier/prettier': 'warn',
@@ -22,36 +14,55 @@ module.exports = {
     'object-shorthand': 'warn',
     'no-useless-rename': 'warn',
     'prefer-arrow-callback': 'error',
-    '@typescript-eslint/no-floating-promises': 'warn',
-    '@typescript-eslint/no-useless-constructor': 'error',
-    '@typescript-eslint/no-misused-promises': 'error', // warn for `if (promise)` and `forEach(async () => ...)` (note: forEach takes void func)
-    '@typescript-eslint/prefer-nullish-coalescing': ['warn', { forceSuggestionFixer: true }],
-    '@typescript-eslint/prefer-optional-chain': 'warn',
-    '@typescript-eslint/unbound-method': 'error', // Warn for unbound this of `foo(classInstance.method)` (`.bind(instance)` is required)
 
     /* Override recommended */
-    'no-constant-condition': ['error', { "checkLoops": false }],
-    '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true, argsIgnorePattern: '^_' }],
-
-    /* Disable rules in recommended */
-    // Some API needs snake case type, and rarely violated
-    '@typescript-eslint/camelcase': 'off',
-    // Too explicit
-    '@typescript-eslint/explicit-function-return-type': 'off',
-    // Empty function is used to match to interface definition while it is actually no-op.
-    '@typescript-eslint/no-empty-function': 'off',
-    // Rewriting `interface Foobar extends Baz {}` to `type Foobar = Baz` is blah
-    '@typescript-eslint/no-empty-interface': 'off',
-    // Too strict
-    '@typescript-eslint/no-non-null-assertion': 'off',
-    // Allow hoisting
-    '@typescript-eslint/no-use-before-define': 'off',
+    'no-constant-condition': ['error', { "checkLoops": false }], // Allow `while (true)`
   },
   overrides: [
     {
-      files: ['*.js'],
+      files: ['*.js', '*.jsx'],
       rules: {
         'no-undef': 'error' // No TypeScript here
+      }
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        'plugin:@typescript-eslint/eslint-recommended',
+        'plugin:@typescript-eslint/recommended',
+        "prettier/@typescript-eslint",
+      ],
+      // You need specify parser, add this to your config:
+      // parserOptions: {
+      //   sourceType: 'module',
+      //   project: './tsconfig.json',
+      //   // https://github.com/typescript-eslint/typescript-eslint/issues/251
+      //   tsconfigRootDir: __dirname,
+      // },
+      rules: {
+        '@typescript-eslint/no-floating-promises': 'warn',
+        '@typescript-eslint/no-useless-constructor': 'error',
+        '@typescript-eslint/no-misused-promises': 'error', // warn for `if (promise)` and `forEach(async () => ...)` (note: forEach takes void func)
+        '@typescript-eslint/prefer-nullish-coalescing': ['warn', { forceSuggestionFixer: true }],
+        '@typescript-eslint/prefer-optional-chain': 'warn',
+        '@typescript-eslint/unbound-method': 'error', // Warn for unbound this of `foo(classInstance.method)` (`.bind(instance)` is required)
+
+        /* Override recommended */
+        '@typescript-eslint/no-unused-vars': ['warn', { ignoreRestSiblings: true, argsIgnorePattern: '^_' }],
+
+        /* Disable rules in recommended */
+        // Some API needs snake case type, and rarely violated
+        '@typescript-eslint/camelcase': 'off',
+        // Too explicit
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        // Empty function is used to match to interface definition while it is actually no-op.
+        '@typescript-eslint/no-empty-function': 'off',
+        // Rewriting `interface Foobar extends Baz {}` to `type Foobar = Baz` is blah
+        '@typescript-eslint/no-empty-interface': 'off',
+        // Too strict
+        '@typescript-eslint/no-non-null-assertion': 'off',
+        // Allow hoisting
+        '@typescript-eslint/no-use-before-define': 'off',
       }
     },
     {
